c9bb689db231eee4c3adf88ec18b0f0217c8e1ec
Nitin Gupta <nitin.z.gupta@intel.com>
2014-04-22 12:24:16 -0700

    Tail file and send to client line-by-line

--- index.js
+++ index.js
@@ -6,11 +6,22 @@ app.use(express.static(__dirname + '/public'));
 
 var io = require('socket.io').listen(app.listen(port));
 
+function parseLine(line) {
+    console.log(line);
+    return line;
+}
+
 io.sockets.on('connection', function(socket) {
     socket.on('client:ready', function(data) {
         console.log("client ready now");
-        socket.emit('data:filename', "pin.log")
+        socket.emit('data:filename', "pin.log");
+        Tail = require('tail').Tail;
+        tail = new Tail("pin.log");
+        tail.on("line", function(data) {
+            var parsed = parseLine(data);
+            socket.emit('data', parsed)
+        });
     });
-});
 
-console.log("Listening on port " + port);
+    console.log("Listening on port " + port);
+});
--- public/js/iohandler.js
+++ public/js/iohandler.js
@@ -1,16 +1,20 @@
-function handleData(data) {
-    console.log("handleData called with data: " + data);
+var view;
+
+function recvFileName(fname) {
+    console.log("filename received: " + fname);
+    $("#heading").text(fname);
 }
 
-function recvFileName(data) {
-    console.log("filename received: " + data);
-    $("#heading").text(data);
+function recvData(data) {
+    //console.log(data);
+    view.append(data + "<br />");
 }
 
 window.onload = function() {
     var socket = io.connect('http://localhost:8080');
-    var content = document.getElementById("content");
+    view = $("#content");
 
     socket.on('data:filename', recvFileName);
+    socket.on('data', recvData)
     socket.emit('client:ready');
 }

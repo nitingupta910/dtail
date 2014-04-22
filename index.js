3ebb2cc2077d20ecc8e10fc4786c73623bd1fbe7
Nitin Gupta <nitin.z.gupta@intel.com>
2014-04-22 12:42:23 -0700

    Accept filename as command line arg

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 var express = require("express");
 var app = express();
-var port = 8080;
+var port = 8080
 
-app.use(express.static(__dirname + '/public'));
-
-var io = require('socket.io').listen(app.listen(port));
+// Parse command-line args
+var argv = require('minimist')(process.argv.slice(2));
+var logfile = argv._[0];
+//console.log("GOT ARGV: " + argv._);
 
 function parseLine(line) {
     console.log(line);
     return line;
 }
 
+// Set static content path
+app.use(express.static(__dirname + '/public'));
+
+// Attach socket to express app
+var io = require('socket.io').listen(app.listen(port));
+
+// Attach functions to socket events
 io.sockets.on('connection', function(socket) {
     socket.on('client:ready', function(data) {
         console.log("client ready now");
-        socket.emit('data:filename', "pin.log");
+        socket.emit('data:filename', logfile);
         Tail = require('tail').Tail;
-        tail = new Tail("pin.log");
+        tail = new Tail(logfile);
         tail.on("line", function(data) {
             var parsed = parseLine(data);
             socket.emit('data', parsed)

7ac9e9ebbe5247902ebe93d7a3025b78af2cf8d4
Nitin Gupta <nitin.z.gupta@intel.com>
2014-04-22 11:43:56 -0700

    Send filename over to client
    also add jquery as dependency

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection', function(socket) {
-    socket.emit('message', {
-        message: 'welcome to the chat'
-    });
-    socket.on('send', function(data) {
-        io.sockets.emit('message', data);
+    socket.on('client:ready', function(data) {
+        console.log("client ready now");
+        socket.emit('data:filename', "pin.log")
     });
 });
 

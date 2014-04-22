011822ec7106f302c17ac4143f02ff489c6a529a
Nitin Gupta <nitin.z.gupta@intel.com>
2014-04-22 13:07:37 -0700

    Use always-tail instead of tail nodejs module
    from always-tail project site:
    
    "It differs from other tail modules in that it survives truncates,
    file rollovers (e.g. mv /var/log/test /var/log/test.1), and unlink."

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require("express");
+var Tail = require('always-tail');
+var fs = require('fs');
 var app = express();
 var port = 8080
 
 // Parse command-line args
 var argv = require('minimist')(process.argv.slice(2));
-var logfile = argv._[0];
-//console.log("GOT ARGV: " + argv._);
+var filename = argv._[0];
+
+// Attach file to tail
+if (!fs.existsSync(filename)) fs.writeFileSync(filename, "");
+var tail = new Tail(filename, '\n');
 
 function parseLine(line) {
     console.log(line);
@@ -22,14 +27,20 @@ var io = require('socket.io').listen(app.listen(port));
 io.sockets.on('connection', function(socket) {
     socket.on('client:ready', function(data) {
         console.log("client ready now");
-        socket.emit('data:filename', logfile);
-        Tail = require('tail').Tail;
-        tail = new Tail(logfile);
-        tail.on("line", function(data) {
+        socket.emit('data:filename', filename);
+
+        // Setup tail callbacks
+        tail.on('line', function(data) {
             var parsed = parseLine(data);
             socket.emit('data', parsed)
         });
-    });
 
-    console.log("Listening on port " + port);
+        tail.on('error', function(data) {
+            console.log('tail error: ', data);
+        });
+
+        tail.watch();
+
+        console.log("Listening on port " + port);
+    });
 });

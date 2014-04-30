dca1888836d58fa0c0bdebf5e306b13dc63d0a16
Nitin Gupta <nitin.z.gupta@intel.com>
2014-04-29 18:31:57 -0700

    x-axis scrolling

--- public/js/iohandler.js
+++ public/js/iohandler.js
@@ -2,12 +2,23 @@
 //var plotData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 var n = 40;
-var random = d3.random.normal(0, .2);
-var plotData = d3.range(n).map(random);
+//var random = d3.random.normal(0, .2);
+//var plotData = d3.range(n).map(random);
+var plotData = [];
+for (i = 0; i < n; i++) {
+    plotData.push(0);
+}
+//var plotData = [400, 900, 1000, 300, 4000];
 
-var x;
+var svg;
 var line;
 var path;
+var xAxis;
+var xAxisView;
+var xScaleView;
+
+var width;
+var eventNumber = 0;
 
 function initView() {
     var margin = {
@@ -15,27 +26,27 @@ function initView() {
         right: 20,
         bottom: 20,
         left: 40
-    },
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+    };
+    width = 960 - margin.left - margin.right;
+    var height = 500 - margin.top - margin.bottom;
 
-    x = d3.scale.linear()
-        .domain([0, n - 1])
+    xScale = d3.scale.linear()
+        .domain([0, n])
         .range([0, width]);
 
-    var y = d3.scale.linear()
-        .domain([-1, 1])
+    var yScale = d3.scale.linear()
+        .domain([0, 4096])
         .range([height, 0]);
 
     line = d3.svg.line()
         .x(function(d, i) {
-            return x(i);
+            return xScale(i);
         })
         .y(function(d, i) {
-            return y(d);
+            return yScale(d);
         });
 
-    var svg = d3.select("body").append("svg")
+    svg = d3.select("body").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
@@ -47,14 +58,30 @@ function initView() {
         .attr("width", width)
         .attr("height", height);
 
-    svg.append("g")
+    xScaleView = d3.scale.linear()
+        .domain([eventNumber - n, eventNumber])
+        .range([0, width]);
+
+    xAxis = d3.svg.axis()
+        .scale(xScaleView)
+        .orient("bottom")
+        .tickFormat(function(d) {
+            if (d < 0) {
+                return ""
+            } else {
+                return d;
+            }
+        })
+        .ticks(10);
+
+    xAxisView = svg.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + y(0) + ")")
-        .call(d3.svg.axis().scale(x).orient("bottom"));
+        .attr("transform", "translate(0, " + yScale(0) + ")")
+        .call(xAxis);
 
     svg.append("g")
         .attr("class", "y axis")
-        .call(d3.svg.axis().scale(y).orient("left"));
+        .call(d3.svg.axis().scale(yScale).orient("left"));
 
     path = svg.append("g")
         .attr("clip-path", "url(#clip)")
@@ -70,29 +97,34 @@ function recvFileName(fname) {
 }
 
 function redraw() {
+    xScaleView
+        .domain([eventNumber - n, eventNumber])
+        .range([0, width]);
+
     // redraw the line, and slide it to the left
     path
+        .datum(plotData)
         .attr("d", line)
         .attr("transform", null)
         .transition()
         .duration(100)
         .ease("linear")
-        .attr("transform", "translate(" + x(-1) + ",0)");
+        .attr("transform", "translate(" + xScale(-1) + ",0)");
+
+    // slide the x-axis left
+    xAxisView.transition()
+        .duration(100)
+        .ease("linear")
+        .call(xAxis);
 
     // pop the old data point off the front
     plotData.shift();
 }
 
 function recvData(data) {
-    //console.log(data);
-    //var rval = Math.random();
-    //console.log(rval);
-    //plotData.push(rval);
-    var rval = random();
-    console.log(rval);
-    plotData.push(rval);
+    eventNumber++;
+    plotData.push(1024);
     redraw();
-    //view.append(data + "<br />");
 }
 
 window.onload = function() {

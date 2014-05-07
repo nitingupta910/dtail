38489f22f5b4ee97e724d050381550a0c9c6132e
Nitin Gupta <nitin.z.gupta@intel.com>
2014-05-07 14:39:46 -0700

    test case for multi-plots

--- tests/testlog.js
+++ tests/testlog.js
@@ -2,17 +2,32 @@ var fs = require('fs');
 var filename = process.argv[2];
 var maxValue = 4096;
 
-function tick() {
-    //var csv = "3,545,22,113";
-    var csv = Math.floor(Math.random() * maxValue);
-    var str = "EVENT:" + csv;
-    fs.appendFile(filename, str + "\n", function(err) {
+function appendToFile(filename, str) {
+    fs.appendFile(filename, str, function(err) {
         if (err) {
             console.log(err);
         } else {
-            console.log(str);
+            process.stdout.write(str);
         }
     });
-
 }
-setInterval(tick, 1000);
+
+function singleEvent() {
+    //var csv = "3,545,22,113";
+    var csv = Math.floor(Math.random() * maxValue);
+    var str = "EVENT:" + csv + "\n";
+    appendToFile(filename, str);
+}
+
+function multiEvents() {
+    var maxEvents = 4;
+    var eventNum = Math.floor(Math.random() * maxEvents);
+    var eventStr = "EVENT[" + eventNum + "]:";
+
+    var csv = Math.floor(Math.random() * maxValue);
+    var str = eventStr + csv + "\n";
+    appendToFile(filename, str);
+}
+
+//setInterval(singleEvent, 1000);
+setInterval(multiEvents, 1000);
